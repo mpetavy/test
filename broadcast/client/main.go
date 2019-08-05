42d56dcaf3758a994ccc7d47d97ac5be9969072d
petavy@gmx.net <petavy@gmx.net>
2019-08-05 22:22:00 +0200

    broadcast

--- broadcast/client/main.go
+++ broadcast/client/main.go
@@ -1,24 +1,52 @@
 package main
 
 import (
-	"fmt"
 	"log"
 	"net"
 	"time"
 )
 
-func getBroadcast() {
-	mask := net.CIDRMask(24, 32)
-	ip := net.IP([]byte{192, 168, 1, 3})
-
-	broadcast := net.IP(make([]byte, 4))
-	for i := range ip {
-		broadcast[i] = ip[i] | ^mask[i]
+func FindActiveIPs() ([]string, error) {
+	var addresses []string
+	// list system network interfaces
+	// https://golang.org/pkg/net/#Interfaces
+	intfs, err := net.Interfaces()
+	if err != nil {
+		return nil, err
 	}
+	// mapping between network interface name and index
+	// https://golang.org/pkg/net/#Interface
+	for _, intf := range intfs {
+		// skip down interface & check next intf
+		if intf.Flags&net.FlagUp == 0 {
+			continue
+		}
+		// skip loopback & check next intf
+		if intf.Flags&net.FlagLoopback != 0 {
+			continue
+		}
+		// list of unicast interface addresses for specific interface
+		// https://golang.org/pkg/net/#Interface.Addrs
+		addrs, err := intf.Addrs()
+		if err != nil {
+			return nil, err
+		}
+		// network end point address
+		// https://golang.org/pkg/net/#Addr
+		for _, addr := range addrs {
+			// if for windows may need to type switch
 
-	fmt.Println(broadcast)
+			// type assertion to access Addr interface
+			// underlying IPNet IP method
+			if addr == nil || addr.(*net.IPNet).IP.IsLoopback() {
+				continue
+			}
+			// append active interfaces
+			addresses = append(addresses, addr.String())
+		}
+	}
+	return addresses, nil
 }
-
 
 func main() {
 	c, err := net.ListenPacket("udp", ":0")
@@ -27,22 +55,54 @@ func main() {
 	}
 	defer c.Close()
 
-	dst, err := net.ResolveUDPAddr("udp", "255.255.255.255:8032")
+	ips, err := FindActiveIPs()
 	if err != nil {
-		log.Fatal(err)
+		panic(err)
 	}
 
-	if _, err := c.WriteTo([]byte("hello from client"), dst); err != nil {
-		log.Fatal(err)
-	}
-	b := make([]byte, 512)
-	c.SetReadDeadline(time.Now().Add(time.Second * 1))
-	for {
-		n, peer, err := c.ReadFrom(b)
+	for _, foundIp := range ips {
+		ipv4Addr, ipv4Net, err := net.ParseCIDR(foundIp)
+
+		ipv4Addr = ipv4Addr.To4()
+
+		if ipv4Addr == nil {
+			continue
+		}
+
+		log.Printf("----------------")
+		log.Printf("ip: %v", ipv4Addr)
+		log.Printf("subnet: %v", ipv4Net)
+
+		ones, bits := ipv4Net.Mask.Size()
+		mask := net.CIDRMask(ones, bits)
+
+		broadcast := net.IP(make([]byte, 4))
+		for i := range ipv4Addr {
+			broadcast[i] = ipv4Addr[i] | ^mask[i]
+		}
+
+		log.Printf("broadcast: %v", broadcast.String())
+
+		dst, err := net.ResolveUDPAddr("udp", broadcast.String()+":8032")
 		if err != nil {
 			log.Fatal(err)
 		}
-		fmt.Println(n, "bytes read from", peer)
-		log.Printf("read: %s\n", string(b[:n]))
+
+		if _, err := c.WriteTo([]byte("hello from client"), dst); err != nil {
+			log.Fatal(err)
+		}
+		b := make([]byte, 512)
+		c.SetReadDeadline(time.Now().Add(time.Second * 1))
+		for {
+			n, peer, err := c.ReadFrom(b)
+			if err != nil {
+				if err, ok := err.(net.Error); ok && err.Timeout() {
+					break
+				} else {
+					log.Fatal(err)
+				}
+			}
+			log.Printf("%d bytes read from %+v: %s\n", n, peer, string(b[:n]))
+		}
 	}
 }
