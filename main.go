4d73e820c439558b92108c2ec458e2d406a9ea25
mpetavy <petavy@gmx.net>
2019-09-23 12:37:39 +0200

    http.DELETE -> resetCfg

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"bytes"
 	"fmt"
 	"github.com/mpetavy/common"
 )
@@ -9,8 +10,21 @@ func init() {
 	common.Init("0.0.0", "2018", "test", "mpetavy", common.APACHE, true, nil, nil, run, 0)
 }
 
+func test(ba *bytes.Buffer) {
+	ba.Reset()
+	ba.WriteString("modified")
+}
+
 func run() error {
 	fmt.Printf("title: %s\n", common.Title())
+
+	ba := []byte("default")
+
+	bb := bytes.NewBuffer(ba)
+
+	test(bb)
+
+	fmt.Printf("%s\n", bb.String())
 
 	return nil
 }
