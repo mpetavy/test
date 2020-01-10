908fa80fabbb8a389c6abbf0897779f3ea7f4798
Marcel Petavy <marcel.petavy@zeiss.com>
2020-01-10 12:28:40 +0100

    common.init, hydra, some i18n, license expire 01.03

--- main.go
+++ main.go
@@ -1,11 +1,12 @@
 package main
 
 import (
+	"fmt"
 	"github.com/mpetavy/common"
 )
 
 func init() {
-	common.Init("0.0.0", "2018", "test", "mpetavy", common.APACHE, true, nil, nil, run, 0)
+	common.Init("0.0.0", "2018", "test", "mpetavy", fmt.Sprintf("https://github.com/mpetavy/%s", common.Title()), common.APACHE, true, nil, nil, run, 0)
 }
 
 func run() error {
