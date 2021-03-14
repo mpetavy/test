793c858213e8d8a4da24e070f9388076ea60d41c
mpetavy <mpetavy>
2021-03-14 22:09:16 +0100

    app.resources with embed.FS

--- main.go
+++ main.go
@@ -9,7 +9,7 @@ import (
 )
 
 func init() {
-	common.Init(false, "0.0.0", "", "2018", "test", "mpetavy", fmt.Sprintf("https://github.com/mpetavy/%s", common.Title()), common.APACHE, nil, nil, run, 0)
+	common.Init(false, "0.0.0", "", "2018", "test", "mpetavy", fmt.Sprintf("https://github.com/mpetavy/%s", common.Title()), common.APACHE, nil, nil, nil, run, 0)
 }
 
 func readDir(dirname string) ([]os.FileInfo, error) {
